bc1e5bb6d08e64539aa20200bc82c79f45e9912a
Lexographics <zbamgamesinfo@gmail.com>
2025-05-25 20:32:08 +0300

    Add initial module

---
+++ go.mod
@@ -0,0 +1,20 @@
+module github.com/Lexographics/logar-web
+
+go 1.23.3
+
+require github.com/Lexographics/logar v1.2.1
+
+replace github.com/Lexographics/logar => ../logar
+
+require (
+	github.com/expr-lang/expr v1.17.2 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/mattn/go-sqlite3 v1.14.24 // indirect
+	github.com/mileusna/useragent v1.3.5 // indirect
+	golang.org/x/crypto v0.36.0 // indirect
+	golang.org/x/text v0.23.0 // indirect
+	gorm.io/driver/sqlite v1.5.7 // indirect
+	gorm.io/gorm v1.25.12 // indirect
+)
---
+++ logarweb.go
@@ -0,0 +1,40 @@
+package logarweb
+
+import (
+	"embed"
+	"net/http"
+
+	"github.com/Lexographics/logar"
+	"github.com/Lexographics/logar/api"
+)
+
+//go:embed build/*
+var staticFiles embed.FS
+
+// ServeHTTP serves the logarweb handler
+//
+// url: url of the logarweb server. e.g. "http://localhost:3000". should not end with a /
+//
+// basePath: should either start with / or be empty (""). e.g. "/logger" or ""
+//
+// returns: http.Handler
+//
+// example:
+//
+// logarweb.ServeHTTP("http://localhost:3000", "/logger", logger)
+//
+// logarweb.ServeHTTP("https://example.com", "/logs", logger)
+func ServeHTTP(url, basePath string, l logar.App) http.Handler {
+	router := http.NewServeMux()
+
+	handler := api.NewHandler(l.(*logar.AppImpl), api.HandlerConfig{
+		BasePath:       basePath,
+		ApiURL:         url + basePath,
+		WebClientFiles: &staticFiles,
+	})
+	handler.Router(router)
+
+	mux := http.NewServeMux()
+	mux.Handle(basePath+"/", http.StripPrefix(basePath, router))
+	return mux
+}
